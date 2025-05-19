9bfda30fed3c7c4785ad48a55b2ce1ac3ddb8f09
devbisme <devb@xess.com>
2025-05-19 05:04:45 -0400

    Don't track version file.

--- version.py
+++ version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev9+g165ed35.d20250509'
-__version_tuple__ = version_tuple = (0, 1, 'dev9', 'g165ed35.d20250509')
+__version__ = version = '0.1.dev25+gcb9a74b.d20250519'
+__version_tuple__ = version_tuple = (0, 1, 'dev25', 'gcb9a74b.d20250519')
